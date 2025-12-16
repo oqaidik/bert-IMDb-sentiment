59ead3536be145c45501f2298e1ebc28ad673230
oqaidik <oqaidik@gmail.com>
2025-12-16 12:33:27 +0100

    Add IMDb dataset class

---
+++ src/dataset.py
@@ -0,0 +1,30 @@
+from torch.utils.data import Dataset
+import torch
+
+class IMDbDataset(Dataset):
+    def __init__(self, texts, labels, tokenizer, max_length=256):
+        self.texts = texts
+        self.labels = labels
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, idx):
+        text = self.texts[idx]
+        label = self.labels[idx]
+
+        encoding = self.tokenizer(
+            text,
+            padding='max_length',
+            truncation=True,
+            max_length=self.max_length,
+            return_tensors='pt'
+        )
+
+        return {
+            "input_ids": encoding["input_ids"].squeeze(0),
+            "attention_mask": encoding["attention_mask"].squeeze(0),
+            "labels": torch.tensor(label, dtype=torch.long)
+        }
